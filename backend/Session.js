65f8e9e88d75e61ce18b395751b8251ebb8d64a3
Lily <46561378+lily-law@users.noreply.github.com>
2020-05-15 16:22:05 +0100

    Remove redundant logging

--- backend/Session.js
+++ backend/Session.js
@@ -8,7 +8,6 @@ function Session(title, io) {
     this.nspToken = uuidv4();
     this.nsp = io.of('/'+this.nspToken);
     this.nsp.on('connection', (socket) => {
-        console.log(socket.id + ' joined');
         socket.emit('players', this.getPlayers());
         this.players[socket.id] = new Player(socket, this);
         socket.on('disconnect', () => {
@@ -22,11 +21,9 @@ Session.prototype.getPlayers = function() {
     return Object.values(this.players).map(({name, id, buzzed}) => ({name, id, buzzed}));
 }
 Session.prototype.buzz = function() {
-    console.log('buzz')
     if (!this.buzzTimeout) {
         this.buzzTimeout = setTimeout(() => {
             const winner = Object.values(this.players).filter(p => p.buzzed).sort((a, b) => a.buzzed - b.buzzed);
-            console.log(winner)
             winner.length > 0 && this.nsp.emit('winner', winner[0].id);
             Object.values(this.players).forEach(p => p.buzzed = null);
             setTimeout(() => {
--- server.js
+++ server.js
@@ -20,20 +20,14 @@ else {
 //build mode
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
     socket.on('nspreq', sessionId => {
         requestNSP(sessionId).then(nsp => {
-            console.log('nsp:'+nsp)
             socket.emit('nsp', nsp);
         }, e => console.error(e));
-    })
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
     });
 });
 
 app.post('/session', jsonParser, function (req, res) {
-    console.log(req.body)
     const sessionId = newSession(req.body.data, io);
     console.log(sessionId+' created!')
     res.send(sessionId);
