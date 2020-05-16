b09f2a24d63188c2ab38a5e3cd931e8f50981ec7
Lily <46561378+lily-law@users.noreply.github.com>
2020-05-16 16:01:45 +0100

    Button feedback sound and vibration

--- client/src/components/BigButton.js
+++ client/src/components/BigButton.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import {beep, stop} from './sound';
 import './BigButton.css';
 
-export default function BigButton({buzz}) {
-    return (<section>
-         <button className="big-button" onMouseDown={buzz}></button>
-    </section>);
+export default function BigButton({buzz, muted}) {
+    const handleButtonPress = e => {
+        e.preventDefault();
+        buzz();
+        !muted && beep();
+        window && window.navigator.vibrate(10000);
+    }
+    const handleButtonRelease = () => {
+        !muted && stop();
+        window && window.navigator.vibrate(0);
+    }
+    useEffect(() => {
+        return () => {
+            !muted && stop();
+            window && window.navigator.vibrate(0);
+        }
+    }, [muted]);
+    return <button className="big-button" onMouseDown={handleButtonPress} onMouseUp={handleButtonRelease}></button>
 }
