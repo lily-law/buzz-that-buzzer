2af245fbc9532c0d5b26bef614c28de2d8c9e8b7
Lily <lily@pop-os.localdomain>
2020-06-25 18:32:27 +0100

    Style and design update

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,26 +1,25 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import { Redirect } from 'react-router-dom';
 import './Landing.css';
 import addIcon from '../images/add.svg';
-import deleteIcon from '../images/cancel.svg';
-import enterIcon from '../images/enter.svg';
+import enterIcon from '../images/enter.svg'
 
-export default function Landing({userData, setUserData}) {
+export default function Landing({userData, updateUserData, sessions, addToSessions, removeSession, test}) {
     const [redirect, setRedirect] = useState();
     const [title, setTitle] = useState('');
-    const [sessions, setSessions] = useState([
-        {title: "Test round one", link: ""}, 
-        {title:"Test round two", link: ""}, 
-        {title:"Another test round", link: ""}
-    ]);
     const userNameRef = useRef();
     const sessionTitleRef = useRef();
-    const createSession = () => {
+    const joinSession = session => {
         if (!userData.name) { 
             userNameRef.current.focus();
             userNameRef.current.style.border = "1px solid red";
         }
-        else if (!title) {
+        else {
+            setRedirect(<Redirect to={'/session/'+session.token+session.title} />);
+        }
+    }
+    const createSession = () => {
+        if (!title) {
             sessionTitleRef.current.focus();
             sessionTitleRef.current.style.border = "1px solid red";
         }
@@ -29,24 +28,21 @@ export default function Landing({userData, setUserData}) {
             xhr.open('POST', '/session');
             xhr.setRequestHeader('Content-Type', 'application/json');
             xhr.onload = function() {
-                setRedirect(<Redirect to={'/session/'+xhr.responseText} />);
+                const token = xhr.responseText.substr(0, 36);
+                addToSessions([{title, token}]);
+                joinSession({title, token});
             };
             xhr.send(JSON.stringify({data: title}));             
         }
     };
     const handleSetUserName = e => {
         userNameRef.current.style.border = "none";
-        setUserData({...userData, name: e.target.value});
+        updateUserData({...userData, name: e.target.value});
     };
     const handleSetTitle = e => {
         sessionTitleRef.current.style.border = "none";
         setTitle(e.target.value);
     }
-
-    useEffect(() => {
-        // TODO check url for session
-        // check localstorage for previous sessions
-    }, []);
     return (<>
         {redirect && redirect}
         <main className="landing">
@@ -59,15 +55,12 @@ export default function Landing({userData, setUserData}) {
                 <input ref={userNameRef} className="setup__user-name" type="text" placeholder="Name/Alias" onChange={handleSetUserName} value={userData.name} />
                 <div className="setup__sessions">
                     {sessions.length > 0 && <>
-                        <button className="sessions__delete">
-                            <img src={deleteIcon} alt="x" />
-                        </button>
                         <ul className="sessions__list">
-                            {sessions.map(sess => <li key={sess.url}>{sess.title}</li>)}
+                            {sessions.map(sess => <li key={sess.token} onClick={() => joinSession(sess)}>
+                                {sess.title}
+                                <img src={enterIcon} alt="" />
+                            </li>)}
                         </ul>
-                        <button className="sessions__enter">
-                            <img src={enterIcon} alt="Enter" />
-                        </button>
                     </>}
                 </div>
                 <div className="setup__new-session">
