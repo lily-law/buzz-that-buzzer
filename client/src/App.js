e822838c7e372d136386831d397e3c2608970524
Lily <lily@pop-os.localdomain>
2020-06-25 18:31:40 +0100

    Store and retrieve userData and sessions

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -6,23 +6,73 @@ import {
 } from "react-router-dom";
 import Landing from './pages/Landing';
 import Session from './pages/Session';
+import {writeToStore, readFromStore} from './controllers/storage';
 import './App.css';
 
 function App() {
+
   const [userData, setUserData] = useState({
-    name: ''
+    name: ""
   });
-  useEffect(() => {
-    // TODO check localstorage for userData
+  const [sessions, setSessions] = useState([]);
+  const addToSessions = (arr, stored) => {
+    const usedTokens = [];
+    const isUniqueToken = (a, c) => {
+      if (usedTokens.includes(c.token)) {
+        return a
+      }
+      else {
+        usedTokens.push(c.token);
+        return [...a, c]
+      }
+    }
+    let newSessions = [...arr, ...sessions].reduce(isUniqueToken, []);
+    !stored && writeToStore("sessions", newSessions);
+    setSessions(newSessions);
+  };
+  const removeSession = session => {
+    const newSessions = sessions.filter(sess => sess.url === session.url);
+    writeToStore("sessions", newSessions);
+    setSessions(newSessions);
+  }
+  const updateUserData = (data, stored) => {
+    const newData = {...userData, ...data};
+    !stored && writeToStore("userData", newData);
+    setUserData(newData);
+  };
+  useState(() => {
+    const addStoredUserData = async () => {
+      if (typeof(Storage) !== "undefined") {
+          // TODO status('checking for stored user data')
+          const userData = await readFromStore("userData");
+          // TODO clearStatus('checking for stored user data')
+          if (userData) {
+              updateUserData(userData);
+          }
+      }
+    };
+    addStoredUserData();
+    const addStoredSessions = async () => {
+      if (typeof(Storage) !== "undefined") {
+          // TODO status('checking for stored sessions')
+          const sessions = await readFromStore("sessions");
+          // TODO clearStatus('checking for stored sessions')
+          if (sessions) {
+            addToSessions(sessions, true);
+          }
+      }
+    };
+    addStoredSessions();
   }, []);
+  
   return (
     <Router>
       <Switch>
         <Route path="/session/:id">
-          <Session {...{userData, setUserData}} />
+          <Session {...{userData, updateUserData, addToSessions, sessions}} />
         </Route>
         <Route path="/">
-          <Landing {...{userData, setUserData}} />
+          <Landing {...{userData, updateUserData, sessions, addToSessions, removeSession}} />
         </Route>
       </Switch>
     </Router>
