3f0207dc2271da96eff3e809e651c27513fad7ce
Lily <46561378+lily-law@users.noreply.github.com>
2020-06-27 12:22:54 +0100

    Development (#13)
    * Fix client socket route by making relative
    
    * Remove redundant logging
    
    * Setup routes
    - Add some styles
    - Improve UX flow
    
    * Button feedback sound and vibration
    
    * Use both touch and mouse events on button press
    - Fix incorrect oscillator type in sound.js
    
    * Session dieIfInactive
    
    * Basic share links/copy setup
    
    * Make sessions revive on request
    - Fix killing sessions
    - Add title to url data
    - Remove extra Session.id field
    - Make sound.js load after user interaction
    
    * Make sound not disappear too quick
    - Change/clean up connetcion flow in Session
    
    * Improve session self timeouts and dying
    
    * Fix Cannot assign to read only property 'exports'
    - replace module.exports with export
    
    * Make sound beeps fixed length
    
    * Set shareLinks isOpen to false after navigator
    
    * Add icon svgs
    
    * Implement theme
    - Apply styles
    - Adjust layout
    - Edit manifest
    
    * Update README
    
    * Retheme (#9)
    
    * Replace/update icon svgs
    
    * Change Landing design and layout
    
    * Validate Landing inputs
    
    * Store and retrieve userData and sessions
    
    * Style and design update
    
    Co-authored-by: Lily <lily@pop-os.localdomain>
    
    * Make landing height fit
    
    * Fix name not storing
    
    * Update Session.js
    
    * Build off feedback (#10)
    
    * Check for userdata on session load
    - Break up single useEffect monster in Session
    
    * Make space work for buzzer press
    
    * Catch throw in storage
    
    * Make player board work better on smaller devices
    
    * Add style for buzz on space press
    
    * Handle enter keypress on landing inputs
    
    * Fix mobile keyboard affecting landing layout
    
    * Change sessions list to buttons
    
    Co-authored-by: Lily <lily@pop-os.localdomain>
    
    * Enhancements (#11)
    
    * Prevent buzzing over self
    - Remove highlight on button when pressed
    
    * Remove unnessary throw
    
    * Further break down logic in session's useEffects
    
    * Sync user colours
    - Server side random colour generation
    
    * Size buttons and inputs better
    
    Co-authored-by: Lily <lily@pop-os.localdomain>
    
    * Update Player.js
    
    * AudioContext fix for Safari
    
    Co-authored-by: Lily <lily@pop-os.localdomain>

--- client/src/components/sound.js
+++ client/src/components/sound.js
@@ -1,18 +1,25 @@
+
 export default function beep(length = 250, onFinish = () => null, options = {sound: true, vibrate: true}) {
-    const ctx = new AudioContext();
-    options.vibrate && window && window.navigator.vibrate(length);
-    if (options.sound) {
-        let oscillators = [new Oscil("triangle", 300, ctx), new Oscil("sawtooth", 200, ctx), new Oscil("square", 400, ctx)];
-        oscillators.forEach(o => {
-            o.oscillator.start(0);
-            o.oscillator.stop(ctx.currentTime + length);
-        });
+    var AudioContext = window.hasOwnProperty("AudioContext") && window.AudioContext || window.webkitAudioContext || null;
+    if (AudioContext) {
+        const ctx = new AudioContext();
+        options.vibrate && window && window.navigator && window.navigator.vibrate && window.navigator.vibrate(length);
+        if (options.sound) {
+            let oscillators = [new Oscil("triangle", 300, ctx), new Oscil("sawtooth", 200, ctx), new Oscil("square", 400, ctx)];
+            oscillators.forEach(o => {
+                o.oscillator.start(0);
+                o.oscillator.stop(ctx.currentTime + length);
+            });
+        }
+        setTimeout(() => {
+            ctx.close();
+            options.vibrate && window && window.navigator && window.navigator.vibrate && window.navigator.vibrate(0);
+            onFinish();
+        }, length);
     }
-    setTimeout(() => {
-        ctx.close();
-        window && window.navigator.vibrate(0);
+    else {
         onFinish();
-    }, length);
+    }
 }
 
 function Oscil(type, freq, context) {
